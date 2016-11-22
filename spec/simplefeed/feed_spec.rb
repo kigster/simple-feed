3cfbfedd72a137d74c2bd337028719ca79a2bf95
Konstantin Gredeskoul <kig@reinvent.one>
2016-11-21 19:44:49 -0800

    Adding a spec for pre instantiated provider

--- spec/simplefeed/feed_spec.rb
+++ spec/simplefeed/feed_spec.rb
@@ -21,6 +21,16 @@ describe 'SimpleFeed::Feed' do
     it('sets max_size') { expect(feed.max_size).to eq(10) }
     it('sets provider proxy') { expect(feed.provider.class).to eq(SimpleFeed::ProviderProxy) }
     it('sets proxys provider') { expect(feed.provider.provider.class).to eq(SimpleFeed::MockProvider) }
+
+    context 'with a pre configured provider' do
+      let(:pre_instantiated_provider) { SimpleFeed::ProviderProxy.from(SimpleFeed::Fixtures.mock_provider_props).provider }
+      let(:provider) { SimpleFeed::ProviderProxy.from(pre_instantiated_provider).provider }
+
+      it 'should be one and the same' do
+        expect(pre_instantiated_provider).to eq(provider)
+      end
+
+    end
   end
 
 end
