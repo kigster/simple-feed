5806b4866ee89c4c2bbf0c604b496339a7424335
Konstantin Gredeskoul <kig@reinvent.one>
2016-11-29 01:31:28 -0800

    Beautiful example in /examples

--- examples/redis-feed.rb
+++ examples/redis-feed.rb
@@ -17,44 +17,41 @@ require 'simplefeed'
 require_relative 'response_formatter'
 
 @feed = SimpleFeed.define(:news) do |f|
-  f.provider = SimpleFeed.provider(:redis, redis: -> { Redis.new }, pool_size: 2)
+  f.provider = SimpleFeed.provider(:redis,
+                                   redis: -> { Redis.new },
+                                   pool_size: 10,
+                                   batch_size: 10)
 end
 
 # You can pass number of users on the command line 
-@number_of_users = ARGV[0] ? ARGV[0].to_i :  1
+@number_of_users = ARGV[0] ? ARGV[0].to_i : 1
 @users           = Array.new(@number_of_users) { rand(200_000_000...800_000_000) }
-
-@ua = @feed.for(@users)
+@ua              = @feed.users_activity(@users)
 
 Formatter.header
+Formatter.with_timing(ua: @ua) do
 
-Formatter.print('___________________________________________________________________________', user_id: nil, pp: true) do
+  p('#wipe entire feed for %user%')             { ua.wipe }
+  p("#store value 'hello' for %user%")          { ua.store(value: 'hello', at: Time.now) }
+  p("#store value 'goodbye' for %user%")        { ua.store(value: 'goodbye', at: Time.now) }
+  p("#store value 'goodbye' again %user%")      { ua.store(value: 'goodbye') }
 
-  Formatter.print('#wipe entire feed for %user%', user_id: @users)             { @ua.wipe }
-  
-  Formatter.print("#store value 'hello' for %user%", user_id: @users)          { @ua.store(value: 'hello', at: Time.now) }
-  Formatter.print("#store value 'goodbye' for %user%", user_id: @users)        { @ua.store(value: 'goodbye', at: Time.now) }
-  Formatter.print("#store value 'goodbye' again %user%", user_id: @users)      { @ua.store(value: 'goodbye') }
-  
-  Formatter.print('#total_count for %user% is now', user_id: @users)           { @ua.total_count }
-  Formatter.print('#unread_count for %user% is now', user_id: @users)          { @ua.unread_count }
-  Formatter.print('#last_read for %user% is now', user_id: @users)             { @ua.last_read }
-  
-  Formatter.print('#paginate event page for %user%', pp: true, user_id: @users){ @ua.paginate(page: 1, per_page: 5) }
-  
-  Formatter.print('#unread_count for %user% is now', user_id: @users)          { @ua.unread_count }
-  Formatter.print('#last_read for %user% is now', user_id: @users)             { @ua.last_read }
-  
-  Formatter.print("#remove value 'goodbye' for %user%", user_id: @users)       { @ua.remove(value: 'goodbye') }
-  
-  Formatter.print('#total_count for %user% is now', user_id: @users)           { @ua.total_count }
-  Formatter.print('#unread_count for %user% is now', user_id: @users)          { @ua.unread_count }
-  
-  Formatter.print('#paginate event page for %user%', pp: true, user_id: @users){ @ua.paginate(page: 1, per_page: 5) }
-  Formatter.print('#wipe entire feed for %user%', user_id: @users)             { @ua.wipe }
-  Formatter.print('#paginate event page for %user%', pp: true, user_id: @users){ @ua.paginate(page: 1, per_page: 5) }
-  
-  :TotalTime 
-  
+  p('#total_count for %user% is now')           { ua.total_count }
+  p('#unread_count for %user% is now')          { ua.unread_count }
+  p('#last_read for %user% is now')             { ua.last_read }
+
+  p('#paginate event page for %user%', pp: 1)   { ua.paginate(page: 1, per_page: 5) }
+  p('#unread_count for %user% is now')          { ua.unread_count }
+  p('#last_read for %user% is now')             { ua.last_read }
+
+  p("#remove value 'goodbye' for %user%")       { ua.remove(value: 'goodbye') }
+
+  p('#total_count for %user% is now')           { ua.total_count }
+  p('#unread_count for %user% is now')          { ua.unread_count }
+
+  p('#paginate event page for %user%', pp: 1)   { ua.paginate(page: 1, per_page: 5) }
+  p('#wipe entire feed for %user%')             { ua.wipe }
+  p('#paginate event page for %user%', pp: 1)   { ua.paginate(page: 1, per_page: 5) }
+
 end
 
--- examples/response_formatter.rb
+++ examples/response_formatter.rb
@@ -5,32 +5,63 @@ AwesomePrint.force_colors = true
 class Formatter
   class << self
     def header
-      puts '——————————————————————————————————————————————————————————————————————————'.red
-      puts 'COMMAND DESCRIPTION                                 LATENCY       RESULT   '.red
-      puts '——————————————————————————————————————————————————————————————————————————'.red
-      puts
+      line(1, 1) do
+        puts 'COMMAND DESCRIPTION                                 LATENCY       RESULT   '.red
+      end
     end
 
-    def run
-      t1       = Time.now.to_f
-      result   = yield if block_given?
-      t2       = Time.now.to_f
-      duration = t2 - t1
+    def print_single_line
+      puts '——————————————————————————————————————————————————————————————————————————'.red
+    end
+
+    def line(pre = nil, post = nil)
+      pre.times { puts } if pre
+      if block_given?
+        print_single_line
+        yield
+        print_single_line
+      else
+        print_single_line
+      end
+      post.times { puts } if post
+    end
+
+    def with_timing(**opts, &block)
+      opts.each_pair do |key, value|
+        self.class.instance_eval do
+          attr_accessor key
+        end
+        self.send("#{key}=".to_sym, value)
+      end
+
+      duration, * = run(&block)
+      line(1) do
+        printf '%s', "#{'Total Duration: '.upcase.white}#{'                                   '.cyan}" +
+          sprintf("(%3.3f ms)\n", duration * 1000).yellow
+      end
+    end
+
+    def run(&block)
+      t1           = Time.now.to_f
+      result       = instance_eval(&block)
+      t2           = Time.now.to_f
+      duration     = t2 - t1
       return duration, result
     end
 
-    def print(message = nil, pp: false, user_id: nil, &block)
+    def p(message = nil, pp: false, &block)
       duration, result = run(&block)
-      if user_id
-        user_id          = [user_id] if !user_id.is_a?(Array)
-        user_id.each { |id| single_user_print(message, id, result, duration / user_id.size, pp) }
+      if self.ua && ua.is_a?(SimpleFeed::MultiUserActivity)
+        ua.each { |id| single_user_print(message, id, result, duration / ua.size, pp) }
       else
-        single_user_print(message, nil, result, duration, pp)
+        raise ArgumentError, 'Do what? ' + message
       end
-      
     end
 
+    alias_method :%, :p
+
     def single_user_print(message, user_id, result, duration, pp)
+      # self.user_id = user_id
       message.gsub!(/%user%/, "for user #{user_id.to_s.green.bold}") if user_id
       message = message.split(' ').map { |w| w=~ /^#/ ? w.magenta : w.blue }.join(' ')
       printf '%-140s (%3.3f ms) %s', message, duration*1000, ' ⇨  '.cyan
