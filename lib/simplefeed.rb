038a33b4869d13ac2ef508f88169d2e864025fa9
Wan Qi Chen <wan@wanki.moe>
2017-05-05 21:26:08 +0400

    Silence Hashie warnings
    Hashie is raising the warning `WARN -- : You are setting a key that conflicts with a built-in method Hashie::Mash#keys defined in Hash. This can cause unexpected behavior when accessing the key via as a property. You can still access the key via the #[] method.` more than 30 times, flooding the logs.

--- lib/simplefeed.rb
+++ lib/simplefeed.rb
@@ -1,6 +1,8 @@
 require 'hashie/extensions/symbolize_keys'
 require 'simplefeed/version'
 
+Hashie.logger = Logger.new(nil)
+
 ::Dir.glob(::File.expand_path('../simplefeed/*.rb', __FILE__)).each { |f| require_relative(f) }
 
 require 'simplefeed/providers/redis'
